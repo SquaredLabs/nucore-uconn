29c1a1541f15d5fa240cfd3d454c769ac274fb23
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2018-10-24 19:17:19 +0200

    Rename column to full_price_cancellation so it fits into Oracle <= 30 limit (#1740)
    TECH TASK: Rename column to full_price_cancellation so it fits into Oracle <= 30 limit

--- app/services/price_policy_updater.rb
+++ app/services/price_policy_updater.rb
@@ -68,7 +68,7 @@ class PricePolicyUpdater
       :unit_cost,
       :unit_subsidy,
     ].tap do |attributes|
-      attributes << :charge_full_price_on_cancellation if SettingsHelper.feature_on?(:charge_full_price_on_cancellation)
+      attributes << :full_price_cancellation if SettingsHelper.feature_on?(:charge_full_price_on_cancellation)
     end
   end
 
