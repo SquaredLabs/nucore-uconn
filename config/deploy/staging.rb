e42efd9caa7f503b5e704d3e7fb14805a0129d31
Vladimir Andrijevikj <vandrijevik@users.noreply.github.com>
2019-08-09 15:56:09 +0200

    Reformat and alphabetize Capistrano deployment config (#2037)

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
-server "test.cider.core.uconn.edu", user: "deploy", roles: %w(web app db)
-set :deploy_to, "/home/deploy/workspace/deploy"
-set :rails_env, "stage"
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
+set :rails_env, "stage"
+
+server "tablexi-shared02.txihosting.com", user: "nucore", roles: %w(web app db)
+set :deploy_to, "/home/nucore/nucore.stage.tablexi.com"
