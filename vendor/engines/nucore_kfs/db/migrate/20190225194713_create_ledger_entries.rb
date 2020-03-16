37a2d3bbbea3448a314d8f31cd4a36ddb6e1183a
Joseph O'Shea <joshea0@gmail.com>
2020-03-16 13:00:47 -0400

    fix: specify Rails version in migration

--- vendor/engines/nucore_kfs/db/migrate/20190225194713_create_ledger_entries.rb
+++ vendor/engines/nucore_kfs/db/migrate/20190225194713_create_ledger_entries.rb
@@ -1,4 +1,4 @@
-class CreateLedgerEntries < ActiveRecord::Migration
+class CreateLedgerEntries < ActiveRecord::Migration[5.0]
   def change
     create_table :ledger_entries do |t|
       t.integer :batch_sequence_number
